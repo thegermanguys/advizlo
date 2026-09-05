50bd092aea97b9bb7cf12978f5835ee39e72a8ab
imawanish5 <awanishsrivastav5@gmail.com>
2026-09-06 01:04:30 +0200

    Remove accidentally duplicated advizlo/ subfolder

--- advizlo/backend/src/admin/dto/set-verification-status.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsEnum } from 'class-validator';
-import { VerificationStatus } from '@prisma/client';
-
-export class SetVerificationStatusDto {
-  @IsEnum(VerificationStatus)
-  status: VerificationStatus;
-}
